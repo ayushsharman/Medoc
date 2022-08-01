67415f1a3755c82b93139cb90e3691d3c4334231
Ayush <ashsharman123@gmail.com>
2022-08-01 18:19:12 +0530

    New app

---
+++ flappy_bird/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: FlappyBird(),
+      ),
+    );
+
+class FlappyBird extends StatelessWidget {
+  const FlappyBird({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
