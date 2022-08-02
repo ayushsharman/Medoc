1c6e0a9b39202dd414059e10712de3d9b4833e6d
Ayush <ashsharman123@gmail.com>
2022-08-02 21:19:00 +0530

    bird motion

--- flappy_bird/lib/homepage.dart
+++ flappy_bird/lib/homepage.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flappy_bird/bird.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +11,19 @@ class HomePage extends StatefulWidget {
 }
 
 class _nameState extends State<HomePage> {
+  double birdYaxis = 0;
+
+  void jump() {
+    Timer.periodic(
+      Duration(milliseconds: 100),
+      ((timer) {
+        setState(() {
+          birdYaxis -= 0.1;
+        });
+      }),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,9 +31,12 @@ class _nameState extends State<HomePage> {
         children: [
           Expanded(
             flex: 2,
-            child: Container(
-              color: Colors.blue,
-              child: Center(
+            child: GestureDetector(
+              onTap: jump,
+              child: AnimatedContainer(
+                alignment: Alignment(0, birdYaxis),
+                duration: Duration(milliseconds: 0),
+                color: Colors.blue,
                 child: MyBird(),
               ),
             ),
