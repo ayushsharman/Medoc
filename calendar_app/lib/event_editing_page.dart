95f42f26138993ab000fb21e71397b3b7707c8e3
Ayush <ashsharman123@gmail.com>
2022-09-05 08:02:23 +0530

    button

---
+++ calendar_app/lib/event_editing_page.dart
@@ -0,0 +1,18 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class EventEditingPage extends StatefulWidget {
+  final Event? event:
+  EventEditingPage({Key? key}) : super(key: key);
+
+  @override
+  State<EventEditingPage> createState() => _EventEditingPageState();
+}
+
+class _EventEditingPageState extends State<EventEditingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
