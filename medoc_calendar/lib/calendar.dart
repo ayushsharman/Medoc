50dac601167b288dc9a8ce411264b9b3b1870c69
Ayush <ashsharman123@gmail.com>
2022-09-17 19:39:44 +0530

    calendar

--- medoc_calendar/lib/calendar.dart
+++ medoc_calendar/lib/calendar.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class Calendar extends StatelessWidget {
+class Calendar extends StatefulWidget {
   const Calendar({Key? key}) : super(key: key);
 
   @override
+  State<Calendar> createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  CalendarFormat format = CalendarFormat.month;
+  DateTime selectedDay = DateTime.now();
+  DateTime focusedDay = DateTime.now();
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: TableCalendar(
+        firstDay: DateTime(1990),
+        focusedDay: DateTime.now(),
+        lastDay: DateTime(2050),
+        calendarFormat: format,
+        onFormatChanged: (CalendarFormat _format) {
+          setState(() {
+            format = _format;
+          });
+        },
+        startingDayOfWeek: StartingDayOfWeek.monday,
+        onDaySelected: (DateTime selectDay, DateTime focusDay) => setState(() {
+          selectedDay = selectDay;
+          focusedDay = focusDay;
+        }, CalendarStyle()),
+      ),
+    );
   }
 }
