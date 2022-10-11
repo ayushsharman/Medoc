cb7cf85e48dd7e5ae6cd3ce84e811e63e47bf9b9
Ayush <ashsharman123@gmail.com>
2022-10-11 20:11:43 +0530

    events

--- medoc_calendar/lib/event.dart
+++ medoc_calendar/lib/event.dart
@@ -6,3 +6,37 @@ class Event {
   Event({required this.title});
   String toString() => this.title;
 }
+
+
+/* showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text("Add Event"),
+            content: TextFormField(
+              controller: _eventController,
+            ),
+            actions: [
+              TextButton(
+                child: Text("Ok"),
+                onPressed: () {
+                  if (_eventController.text.isEmpty) {
+                  } else {
+                    selectedEvents[selectedDay] = [
+                      Event(title: _eventController.text)
+                    ];
+                  }
+                  Navigator.pop(context);
+                  _eventController.clear();
+                  setState(() {});
+                  return;
+                },
+              ),
+              TextButton(
+                child: Text("Cancel"),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ],
+          ),
+        ),
+        child: 
+    ); */
