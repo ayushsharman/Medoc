84be59f940c77cd80ddd71adbfbeca8394462692
Ayush <ashsharman123@gmail.com>
2022-10-28 19:40:38 +0530

    date

--- medoc_calendar/lib/event.dart
+++ medoc_calendar/lib/event.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:intl/intl.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 
 class AddEvent extends StatefulWidget {
   const AddEvent({Key? key}) : super(key: key);
@@ -95,6 +96,9 @@ class _AddEventState extends State<AddEvent> {
                   ),
                   Divider(),
                   FormBuilderDateTimePicker(
+                    validator: FormBuilderValidators.compose([
+                      FormBuilderValidators.required(),
+                    ]),
                     name: "Date",
                     initialValue: DateTime.now(),
                     fieldHintText: "Add Date",
