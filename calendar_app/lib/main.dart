85398505a46a6caadd0b30651bc77dbb196f1ee0
Ayush <ashsharman123@gmail.com>
2022-10-30 20:39:11 +0530

    gradle fix

--- calendar_app/lib/main.dart
+++ calendar_app/lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:calendar_app/calendar_widget.dart';
-import 'package:calendar_app/event_editing_page.dart';
-import 'package:syncfusion_flutter_calendar/calendar.dart';
 import 'package:flutter/material.dart';
+import 'package:calendar_app/calendar.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,39 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Calendar',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: CalendarWidget(),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(
-            Icons.add,
-            color: Colors.white,
-          ),
-          backgroundColor: Color.fromARGB(255, 33, 95, 128),
-          onPressed: () => Navigator.of(context).push(
-            MaterialPageRoute(builder: (context) => EventEditingPage()),
-          ),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: "Medoc Calendar",
+      home: Calendar(),
     );
   }
 }
--- medoc_calendar/android/app/src/main/kotlin/com/example/medoc_calendar/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.medoc_calendar
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
