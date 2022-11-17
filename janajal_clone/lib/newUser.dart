33849befcc715ecc0664f9cb721a471e520d7071
Ayush <ashsharman123@gmail.com>
2022-11-17 19:28:05 +0530

    newUser login

--- janajal_clone/lib/newUser.dart
+++ janajal_clone/lib/newUser.dart
@@ -15,21 +15,77 @@ class _newUserState extends State<newUser> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Colors.black, //change your color here
+        ),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: Text(
+          'Sign Up',
+          style: TextStyle(
+            color: Colors.grey[600],
+            fontSize: 30,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 60.0),
-              child: Center(
-                child: Container(
-                    width: 200,
-                    height: 150,
-                    child: Image.asset('assets/download.jpg')),
-              ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  width: 200,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15),
+                    child: PhysicalModel(
+                      borderRadius: BorderRadius.circular(25),
+                      color: Colors.white,
+                      elevation: 5.0,
+                      shadowColor: Colors.black,
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20)),
+                            ),
+                            labelText: 'First Name',
+                            hintText: 'Enter your first name'),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 200,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15),
+                    child: PhysicalModel(
+                      borderRadius: BorderRadius.circular(25),
+                      color: Colors.white,
+                      elevation: 5.0,
+                      shadowColor: Colors.black,
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20)),
+                            ),
+                            labelText: 'Last Name',
+                            hintText: 'Enter your last name'),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
             SizedBox(
-              height: 40,
+              height: 20,
             ),
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 15),
@@ -39,17 +95,39 @@ class _newUserState extends State<newUser> {
                 elevation: 5.0,
                 shadowColor: Colors.black,
                 child: TextField(
+                  obscureText: true,
                   decoration: InputDecoration(
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.all(Radius.circular(20)),
                       ),
-                      labelText: 'Email/Phone',
-                      hintText: 'Enter valid email id as abc@gmail.com'),
+                      labelText: 'Mobile',
+                      hintText: 'Enter your mobile number'),
                 ),
               ),
             ),
             SizedBox(
-              height: 40,
+              height: 20,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: PhysicalModel(
+                borderRadius: BorderRadius.circular(25),
+                color: Colors.white,
+                elevation: 5.0,
+                shadowColor: Colors.black,
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                      labelText: 'Email',
+                      hintText: 'Enter your email address'),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
             ),
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 15),
@@ -70,59 +148,30 @@ class _newUserState extends State<newUser> {
               ),
             ),
             SizedBox(
-              height: 40,
+              height: 20,
             ),
             Container(
               margin: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0)),
-                      elevation: 5.0,
-                      shadowColor: Colors.black,
-                      backgroundColor: Colors.blue[800],
-                    ),
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (_) => HomePage()));
-                    },
-                    child: Text(
-                      'Login',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25,
-                      ),
-                    ),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0)),
+                  elevation: 5.0,
+                  shadowColor: Colors.black,
+                  backgroundColor: Colors.blue[800],
+                ),
+                onPressed: () {
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (_) => HomePage()));
+                },
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
                   ),
-                  Container(
-                    height: 50,
-                    width: 200,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30.0)),
-                        elevation: 5.0,
-                        shadowColor: Colors.black,
-                        backgroundColor: Colors.white,
-                      ),
-                      onPressed: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (_) => HomePage()));
-                      },
-                      child: Text(
-                        'Login Via OTP',
-                        style: TextStyle(
-                          color: Colors.blue[800],
-                          fontSize: 25,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+                ),
               ),
             ),
             SizedBox(
