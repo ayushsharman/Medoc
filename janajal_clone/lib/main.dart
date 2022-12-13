b53cb5cc037244f6968436cda806b7db42b2a8fa
Ayush <ashsharman123@gmail.com>
2022-12-13 08:05:56 +0530

    firebase setup

--- janajal_clone/lib/main.dart
+++ janajal_clone/lib/main.dart
@@ -9,22 +9,27 @@ import 'package:janajal_clone/pages/prepaid.dart';
 import 'package:janajal_clone/pages/profile.dart';
 import 'package:janajal_clone/pages/wallet.dart';
 import 'package:janajal_clone/terms.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    initialRoute: 'login',
-    routes: {
-      'login': (context) => loginPage(),
-      'newUser': (context) => const newUser(),
-      'homePage': (context) => const HomePage(),
-      'order': (context) => const OrderPage(),
-      'profile': (context) => const MyProfile(),
-      'wallet': (context) => const MyWallet(),
-      'delivery': (context) => const MyDelivery(),
-      'faq': (context) => const MyFaqList(),
-      'terms': (context) => const MyTerms(),
-      'prepaid': (context) => const PrepaidCard(),
-    },
-  ));
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'login',
+      routes: {
+        'login': (context) => loginPage(),
+        'newUser': (context) => const newUser(),
+        'homePage': (context) => const HomePage(),
+        'order': (context) => const OrderPage(),
+        'profile': (context) => const MyProfile(),
+        'wallet': (context) => const MyWallet(),
+        'delivery': (context) => const MyDelivery(),
+        'faq': (context) => const MyFaqList(),
+        'terms': (context) => const MyTerms(),
+        'prepaid': (context) => const PrepaidCard(),
+      },
+    ),
+  );
 }
