2a53a3f7e99e53397eb21e974c11d57b65afe6bb
Ayush <ashsharman123@gmail.com>
2023-02-01 08:58:01 +0530

    changes

--- source_code/android/app/src/main/kotlin/com/example/active_ecommerce_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.active_ecommerce_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- source_code/lib/other_config.dart
+++
@@ -1,6 +0,0 @@
-
-class OtherConfig{
-  static const bool USE_PUSH_NOTIFICATION = false;
-  static const bool USE_GOOGLE_MAP = false;
-  static const String GOOGLE_MAP_API_KEY = "";
-}
